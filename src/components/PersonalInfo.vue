d818ecc708b7d3d585b584474328099394d59ae3
DESKTOP-MIS84OG\admin <mage568@hotmail.com>
2018-12-25 18:20:38 -0800

    create person info component

---
+++ src/components/PersonalInfo.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="personinfo">
+        adfasdfasfasdfadfasdf
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PersonalInfo",
+    props: {
+
+    }
+}
+</script>
+
+<style>
+
+</style>
+
